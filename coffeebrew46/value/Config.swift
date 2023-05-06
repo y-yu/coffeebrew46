d6b6bc90ff5ca22756738bd617af2404a6f88552
Yoshimura Yuu <yyu@mental.poker>
2023-05-06 23:57:35 +0900

    Add JSON import/export

--- coffeebrew46/value/Config.swift
+++ coffeebrew46/value/Config.swift
@@ -13,6 +13,15 @@ struct Config {
     
     var steamingTimeSec: Double
     
+    enum CodingKeys: String, CodingKey {
+        case coffeeBeansWeight
+        case partitionsCountOf6
+        case waterToCoffeeBeansWeightRatio
+        case firstWaterPercent
+        case totalTimeSec
+        case steamingTimeSec
+    }
+    
     init() {
         coffeeBeansWeight = 30
         partitionsCountOf6 = 3
@@ -26,5 +35,29 @@ struct Config {
 extension Config {
     func totalWaterAmount() -> Double {
         self.coffeeBeansWeight * self.waterToCoffeeBeansWeightRatio
+    }
+}
+
+extension Config: Decodable {
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        coffeeBeansWeight = try values.decode(Double.self, forKey: .coffeeBeansWeight)
+        partitionsCountOf6 = try Double(values.decode(Int.self, forKey: .partitionsCountOf6))
+        waterToCoffeeBeansWeightRatio = try values.decode(Double.self, forKey: .waterToCoffeeBeansWeightRatio)
+        firstWaterPercent = try values.decode(Double.self, forKey: .firstWaterPercent)
+        totalTimeSec = try Double(values.decode(Int.self, forKey: .totalTimeSec))
+        steamingTimeSec = try Double(values.decode(Int.self, forKey: .steamingTimeSec))
+    }
+}
+
+extension Config: Encodable {
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(coffeeBeansWeight, forKey: .coffeeBeansWeight)
+        try container.encode(Int(partitionsCountOf6), forKey: .partitionsCountOf6)
+        try container.encode(waterToCoffeeBeansWeightRatio, forKey: .waterToCoffeeBeansWeightRatio)
+        try container.encode(firstWaterPercent, forKey: .firstWaterPercent)
+        try container.encode(totalTimeSec, forKey: .totalTimeSec)
+        try container.encode(steamingTimeSec, forKey: .steamingTimeSec)
     }
 }
