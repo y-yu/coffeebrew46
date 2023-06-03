72bc290a8a023e4a193ef6af1d96b071c800a2e8
Yoshimura Yuu <yyu@mental.poker>
2023-06-04 00:09:28 +0900

    Add coffee beans from water setting

--- coffeebrew46/view/config/ConfigView.swift
+++ coffeebrew46/view/config/ConfigView.swift
@@ -6,106 +6,127 @@ struct ConfigView: View {
     @EnvironmentObject var viewModel: CurrentConfigViewModel
     
     @State var showTips: Bool = false
+    @State var calculateCoffeeBeansWeightFromWater: Bool = false
+    @State var temporaryWaterAmount: Double = 0.0 {
+        didSet {
+            viewModel.currentConfig.coffeeBeansWeight = temporaryWaterAmount / viewModel.currentConfig.waterToCoffeeBeansWeightRatio
+        }
+    }
+    
     private let timerStep: Double = 1.0
+    private let coffeeBeansWeightMax = 50.0
+    private let coffeeBeansWeightMin = 1.0
     
     var body: some View {
         Form {
             Toggle("config show tips", isOn: $showTips)
             Section(header: Text("config weight settings")) {
-                HStack {
-                    Text("config coffee beans weight")
-                    Text("\(String(format: "%.1f", viewModel.currentConfig.coffeeBeansWeight))g")
+                Toggle(isOn: $calculateCoffeeBeansWeightFromWater) {
+                    TipsView(
+                        showTips,
+                        content: Text("config calculate coffee beans from water"),
+                        tips: Text("config calculate coffee beans from water tips")
+                    )
                 }
-                ButtonSliderButtonView(
-                    maximum: 50.0,
-                    minimum: 0,
-                    sliderStep: 0.5,
-                    buttonStep: 0.1,
-                    isDisable: appEnvironment.isTimerStarted,
-                    target: $viewModel.currentConfig.coffeeBeansWeight
-                )
-                
-                TipsView(
-                    showTips,
-                    content: Text("config water ratio"),
-                    tips: Text("config water ratio tips")
-                )
-                ButtonNumberButtonView(
-                    maximum: 20,
-                    minimum: 5,
-                    step: 1.0,
-                    isDisable: appEnvironment.isTimerStarted,
-                    target: $viewModel.currentConfig.waterToCoffeeBeansWeightRatio
-                )
+                .onChange(of: calculateCoffeeBeansWeightFromWater) { newValue in
+                    if (newValue) {
+                        temporaryWaterAmount = viewModel.currentConfig.totalWaterAmount()
+                    }
+                }
+                !calculateCoffeeBeansWeightFromWater ? AnyView(coffeeBeansWeightSettingView) : AnyView(waterAmountSettingView)
+                VStack {
+                    TipsView(
+                        showTips,
+                        content: Text("config water ratio"),
+                        tips: Text("config water ratio tips")
+                    )
+                    ButtonNumberButtonView(
+                        maximum: 20,
+                        minimum: 5,
+                        step: 1.0,
+                        isDisable: appEnvironment.isTimerStarted,
+                        target: $viewModel.currentConfig.waterToCoffeeBeansWeightRatio
+                    )
+                }
             }
             
             Section(header: Text("config 4:6 settings")) {
-                TipsView(
-                    showTips,
-                    content: HStack {
-                        Text("config 1st water percent")
-                        Text("\(String(format: "%.0f%", viewModel.currentConfig.firstWaterPercent * 100))%")
-                        
-                    },
-                    tips: Text("config 1st water percent tips")
-                )
-                ButtonSliderButtonView(
-                    maximum: 1.0,
-                    minimum: 0.01,
-                    sliderStep: 0.05,
-                    buttonStep: 0.01,
-                    isDisable: appEnvironment.isTimerStarted,
-                    target: $viewModel.currentConfig.firstWaterPercent
-                )
+                VStack {
+                    TipsView(
+                        showTips,
+                        content: HStack {
+                            Text("config 1st water percent")
+                            Text("\(String(format: "%.0f%", viewModel.currentConfig.firstWaterPercent * 100))%")
+                            
+                        },
+                        tips: Text("config 1st water percent tips")
+                    )
+                    ButtonSliderButtonView(
+                        maximum: 1.0,
+                        minimum: 0.01,
+                        sliderStep: 0.05,
+                        buttonStep: 0.01,
+                        isDisable: appEnvironment.isTimerStarted,
+                        target: $viewModel.currentConfig.firstWaterPercent
+                    )
+                }
                 
-                TipsView(
-                    showTips,
-                    content: Text("config number of partitions of later 6"),
-                    tips: Text("config number of partitions of later 6 tips")
-                )
-                ButtonNumberButtonView(
-                    maximum: 10,
-                    minimum: 1,
-                    step: 1.0,
-                    isDisable: appEnvironment.isTimerStarted,
-                    target: $viewModel.currentConfig.partitionsCountOf6
-                )
+                VStack {
+                    TipsView(
+                        showTips,
+                        content: Text("config number of partitions of later 6"),
+                        tips: Text("config number of partitions of later 6 tips")
+                    )
+                    ButtonNumberButtonView(
+                        maximum: 10,
+                        minimum: 1,
+                        step: 1.0,
+                        isDisable: appEnvironment.isTimerStarted,
+                        target: $viewModel.currentConfig.partitionsCountOf6
+                    )
+                }
             }
             
             Section(header: Text("config timer setting")) {
-                HStack {
-                    Text("config total time")
-                    Text((String(format: "%.0f", viewModel.currentConfig.totalTimeSec)))
-                    Text("config sec unit")
+                VStack {
+                    HStack {
+                        Text("config total time")
+                        Text((String(format: "%.0f", viewModel.currentConfig.totalTimeSec)))
+                        Text("config sec unit")
+                        Spacer()
+                    }
+                    ButtonSliderButtonView(
+                        maximum: 300.0,
+                        // If `totalTime` would be going down less than `steamingTime` + its step,
+                        // then `SliderView` will crash so this `steamingTime + timerStep` is needed to avoid crash.
+                        minimum: viewModel.currentConfig.steamingTimeSec + timerStep,
+                        sliderStep: timerStep,
+                        buttonStep: timerStep,
+                        isDisable: appEnvironment.isTimerStarted,
+                        target: $viewModel.currentConfig.totalTimeSec
+                    )
                 }
-                ButtonSliderButtonView(
-                    maximum: 300.0,
-                    // If `totalTime` would be going down less than `steamingTime` + its step,
-                    // then `SliderView` will crash so this `steamingTime + timerStep` is needed to avoid crash.
-                    minimum: viewModel.currentConfig.steamingTimeSec + timerStep,
-                    sliderStep: timerStep,
-                    buttonStep: timerStep,
-                    isDisable: appEnvironment.isTimerStarted,
-                    target: $viewModel.currentConfig.totalTimeSec
-                )
                 
-                HStack {
-                    Text("config steaming time")
-                    Text(String(format: "%.0f", viewModel.currentConfig.steamingTimeSec))
-                    Text("config sec unit")
+                VStack {
+                    HStack {
+                        Text("config steaming time")
+                        Text(String(format: "%.0f", viewModel.currentConfig.steamingTimeSec))
+                        Text("config sec unit")
+                        Spacer()
+                    }
+                    ButtonSliderButtonView(
+                        // This is required to avoid crash.
+                        // If the maximum is `viewModel.totalTime - timerStep` then
+                        // `totalTime`'s slider range could be 300~300 and it will crash
+                        // so that's the why `timerStep` double subtractions are required.
+                        maximum: viewModel.currentConfig.totalTimeSec - timerStep - timerStep > 1 + timerStep ? viewModel.currentConfig.totalTimeSec - timerStep - timerStep : 1 + timerStep,
+                        minimum: 1,
+                        sliderStep: timerStep,
+                        buttonStep: timerStep,
+                        isDisable: appEnvironment.isTimerStarted,
+                        target: $viewModel.currentConfig.steamingTimeSec
+                    )
                 }
-                ButtonSliderButtonView(
-                    // This is required to avoid crash.
-                    // If the maximum is `viewModel.totalTime - timerStep` then
-                    // `totalTime`'s slider range could be 300~300 and it will crash
-                    // so that's the why `timerStep` double subtractions are required.
-                    maximum: viewModel.currentConfig.totalTimeSec - timerStep - timerStep > 1 + timerStep ? viewModel.currentConfig.totalTimeSec - timerStep - timerStep : 1 + timerStep,
-                    minimum: 1,
-                    sliderStep: timerStep,
-                    buttonStep: timerStep,
-                    isDisable: appEnvironment.isTimerStarted,
-                    target: $viewModel.currentConfig.steamingTimeSec
-                )
             }
             Section(header: Text("config json")) {
                 NavigationLink(value: Route.saveLoad) {
@@ -118,6 +139,50 @@ struct ConfigView: View {
         }
         .navigationTitle("Configuration")
         .navigation(path: $appEnvironment.configPath)
+    }
+                             
+    private var coffeeBeansWeightSettingView: some View {
+        VStack {
+            HStack {
+                Text("config coffee beans weight")
+                Text("\(String(format: "%.1f", viewModel.currentConfig.coffeeBeansWeight))g")
+                Spacer()
+            }
+            ButtonSliderButtonView(
+                maximum: coffeeBeansWeightMax,
+                minimum: coffeeBeansWeightMin,
+                sliderStep: 0.5,
+                buttonStep: 0.1,
+                isDisable: appEnvironment.isTimerStarted,
+                target: $viewModel.currentConfig.coffeeBeansWeight
+            )
+        }
+    }
+    
+    private var waterAmountSettingView: some View {
+        // In order to call `didSet` of `temporaryWaterAmount`,
+        // I cannot help but to define this `Binding` manually.
+        // I don't know why it's required.
+        let temporaryWaterAmountBinding: Binding<Double> = Binding(
+            get: { self.temporaryWaterAmount },
+            set: { self.temporaryWaterAmount = $0 }
+        )
+
+        return VStack {
+            HStack {
+                Text("config water amount")
+                Text("\(String(format: "%.0f", temporaryWaterAmount))g")
+                Spacer()
+            }
+            ButtonSliderButtonView(
+                maximum: coffeeBeansWeightMax * viewModel.currentConfig.waterToCoffeeBeansWeightRatio,
+                minimum: coffeeBeansWeightMin * viewModel.currentConfig.waterToCoffeeBeansWeightRatio,
+                sliderStep: 10,
+                buttonStep: 1,
+                isDisable: appEnvironment.isTimerStarted,
+                target: temporaryWaterAmountBinding
+            )
+        }
     }
 }
 
