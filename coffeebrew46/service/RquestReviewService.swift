f09cf1bf7cace5ca716de1deac03d9095257dd29
Yoshimura Yuu <yyu@mental.poker>
2024-01-28 16:41:08 +0900

    Add request review guard

--- coffeebrew46/service/RquestReviewService.swift
+++ coffeebrew46/service/RquestReviewService.swift
@@ -7,19 +7,53 @@ protocol RequestReviewService {
 }
 
 class RequestReviewServiceImpl: RequestReviewService {
-    private let userDefaultKey: String = "requestReviewInfo"
+    private let requestReviewInfoKey: String = "requestReviewInfo"
+    
+    private let requestReviewGuardKey: String = "requestReviewGuard"
     
     private let delta: Double = Double(100 * 24 * 60 * 60) // 100 days
     
     private let waiting: Double = 1.0 // second
     
+    private let minimumTryCount: Int = 3
+    
     func show() -> Void {
+        if beforeCheck() {
+            requestReview()
+        }
+    }
+    
+    private func saveInitGuard() -> Void {
+        RequestReviewGuard(tryCount: 1).toJSON(isPrettyPrint: false).forEach { json in
+            UserDefaults.standard.set(json, forKey: requestReviewGuardKey)
+        }
+    }
+    
+    private func beforeCheck() -> Bool {
+        if let requestReviewGuardJson = UserDefaults.standard.string(forKey: requestReviewGuardKey) {
+            switch RequestReviewGuard.fromJSON(requestReviewGuardJson) {
+            case .success(let requestReviewGuard):
+                RequestReviewGuard(tryCount: requestReviewGuard.tryCount + 1).toJSON(isPrettyPrint: false).forEach { json in
+                    UserDefaults.standard.set(json, forKey: requestReviewGuardKey)
+                }
+                return requestReviewGuard.tryCount >= minimumTryCount
+            case .failure(_):
+                saveInitGuard()
+                return false
+            }
+        } else {
+            saveInitGuard()
+            return false
+        }
+    }
+    
+    private func requestReview() -> Void {
         let now = Date.now
         let appVersion: String = (Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String)!
         
         let requestReviewItem = RequestReviewItem(appVersion: appVersion, requestedDate: now)
         
-        if let requestReviewInfoJson = UserDefaults.standard.string(forKey: userDefaultKey) {
+        if let requestReviewInfoJson = UserDefaults.standard.string(forKey: requestReviewInfoKey) {
             let requestReviewInfoResult = RequestReviewInfo.fromJSON(requestReviewInfoJson)
             requestReviewInfoResult.forEach { requestReviewInfo in
                 if !requestReviewInfo.requestHistory.isEmpty {
@@ -29,7 +63,7 @@ class RequestReviewServiceImpl: RequestReviewService {
                         let updatedRequestReviewInfo = RequestReviewInfo(requestHistory: requestReviewInfo.requestHistory + [requestReviewItem])
                         
                         updatedRequestReviewInfo.toJSON(isPrettyPrint: false).forEach { json in
-                            UserDefaults.standard.set(json, forKey: userDefaultKey)
+                            UserDefaults.standard.set(json, forKey: requestReviewInfoKey)
                             DispatchQueue.main.asyncAfter(deadline: .now() + waiting) {
                                 let scenes = UIApplication.shared.connectedScenes
                                 if let windowScene = scenes.first as? UIWindowScene {
@@ -43,7 +77,7 @@ class RequestReviewServiceImpl: RequestReviewService {
         } else {
             let updatedRequestReviewInfo = RequestReviewInfo(requestHistory: [requestReviewItem])
             updatedRequestReviewInfo.toJSON(isPrettyPrint: false).forEach { json in
-                UserDefaults.standard.set(json,  forKey: userDefaultKey)
+                UserDefaults.standard.set(json,  forKey: requestReviewInfoKey)
     
                 DispatchQueue.main.asyncAfter(deadline: .now() + waiting) {
                     let scenes = UIApplication.shared.connectedScenes
